75c07795decd3e4244e3adb75f40c23d20360395
Thiago Teixeira(Notebook) <thiago3174@gmail.com>
2022-07-04 12:57:14 -0300

    220704

--- typescript-main/dist/app.js
+++ typescript-main/dist/app.js
@@ -2,9 +2,15 @@
 var button = document.getElementById('button');
 var input1 = document.getElementById('input1');
 var input2 = document.getElementById('input2');
-function adicionarNumero(numero1, numero2) {
+function adicionarNumero(numero1, numero2, devePrintar, frase) {
+    var resultado = numero1 + numero2;
+    if (devePrintar) {
+        console.log(frase + resultado);
+    }
     return numero1 + numero2;
 }
+var devePrintar = true;
+var frase = 'O Valor é: ';
 if (button) {
     button.addEventListener('click', function () {
         if (input1 && input2) {
